aca8b55b122da994b5795858f5a268ff2eb5fd2c
digimon <digimon.1740@gmail.com>
2021-03-21 02:49:20 +0900

    라우터 predicate

---
+++ src/main/kotlin/com/example/webflux/fnwebflux/router/Route2.kt
@@ -0,0 +1,26 @@
+package com.example.webflux.fnwebflux.router
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+
+
+@Configuration
+class Route2 {
+
+    @Bean
+    fun hiRoute(): RouterFunction<ServerResponse> =
+        RouterFunctions.route()
+            .GET("/hi", {
+                val userId = it.headers().firstHeader("x-user-id")
+                userId == "tony"
+            }) {
+                ServerResponse.ok().bodyValue("Hi Tony")
+            }
+            .GET("/hi") {
+                ServerResponse.ok().bodyValue("Hi")
+            }
+            .build()
+}
