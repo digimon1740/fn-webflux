d1682a4c10c26a4e0e4c028d3b6015cbace219fa
digimon <digimon.1740@gmail.com>
2021-03-21 02:41:12 +0900

    라우터 등록

---
+++ src/main/kotlin/com/example/webflux/fnwebflux/router/Route1.kt
@@ -0,0 +1,24 @@
+package com.example.webflux.fnwebflux.router
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+
+
+@Configuration
+class Route1 {
+
+    @Bean
+    fun route(): RouterFunction<ServerResponse> =
+        RouterFunctions.route()
+            .GET("/hello") {
+                ServerResponse.ok().bodyValue("Hello World2")
+            }
+            .GET("/hello/{name}") {
+                val message = "Hello ${it.pathVariable("name")}"
+                ServerResponse.ok().bodyValue(message)
+            }
+            .build()
+}
